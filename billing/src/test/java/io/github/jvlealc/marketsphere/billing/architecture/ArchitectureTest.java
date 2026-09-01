71384a4c5f244f3288c7136e4f130f76d9db4f2b
João Vitor Leal de Castro <jv.leal.dev@gmail.com>
2026-09-01 17:58:58 -0300

    test(billing): stop ArchUnit from analyzing test classes

--- billing/src/test/java/io/github/jvlealc/marketsphere/billing/architecture/ArchitectureTest.java
+++ billing/src/test/java/io/github/jvlealc/marketsphere/billing/architecture/ArchitectureTest.java
@@ -1,5 +1,6 @@
 package io.github.jvlealc.marketsphere.billing.architecture;
 
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
@@ -9,7 +10,7 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 /**
  * Impõe as fronteiras hexagonais deste módulo.
  */
-@AnalyzeClasses(packages = ArchitectureTest.ROOT_PACKAGE)
+@AnalyzeClasses(packages = ArchitectureTest.ROOT_PACKAGE, importOptions = ImportOption.DoNotIncludeTests.class)
 class ArchitectureTest {
 
     static final String ROOT_PACKAGE = "io.github.jvlealc.marketsphere.billing";
